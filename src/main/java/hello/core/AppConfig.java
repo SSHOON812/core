76ece33b9e6cabdbb736d4d2c0f1ca01c70efcf7
SSHOON812 <sshoon812@gmail.com>
2022-02-27 01:36:41 +0900

    Spring Study

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,7 +8,10 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration //설정정보
 public class AppConfig {
 
     // 애플리케이션의 실제 동작에 필요한 구현 객체를 생성한다.
@@ -18,18 +21,22 @@ public class AppConfig {
 
     // 객체를 생성하고 관리하면서 의존관계를 연결 해 주는 것을 DI , IOC 컨테이너 
 
-    public MemberService memberService(){
-        return new MemberServiceImpl(memberRepository());
+    @Bean // 스프링 컨테이너에 등록 , 메서드의 이름을 스프링의 이름으로 사용 한다. 빈의 이름은 항상 다른 이름을 부여!
+    public MemberService memberService(){ // 키값
+        return new MemberServiceImpl(memberRepository()); // 밸류 값
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(),  discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
        // return new FixDiscountPolicy();
         return new RateDiscountPolicy();
---
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,69 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationContextBasicFindTest {
+
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName(" 빈 이름으로 조회")
+    void findBeanbyName() {
+
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        //System.out.println("memberService = " + memberService);
+        //System.out.println("memberService.getClass() = " + memberService.getClass());
+
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+
+    @Test
+    @DisplayName(" 이름없이 타입으로 조회")
+    void findBeanByType() {
+
+        MemberService memberService = ac.getBean(MemberService.class);
+        //System.out.println("memberService = " + memberService);
+        //System.out.println("memberService.getClass() = " + memberService.getClass());
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+
+    @Test
+    @DisplayName(" 구체 이름으로 조회")
+    void findBeanbyName2() { // 구현에 의존 하는 코드,
+
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        //System.out.println("memberService = " + memberService);
+        //System.out.println("memberService.getClass() = " + memberService.getClass());
+
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+
+
+    @Test
+    @DisplayName("빈 이름으로 조회x")
+    void findBeanByNameX(){
+    //    MemberService xxxxx =ac.getBean("xxxxxx",MemberService.class);
+
+        assertThrows(NoSuchBeanDefinitionException.class,
+                ()-> ac.getBean("xxxx",MemberService.class));
+
+
+    }
+
+
+
+}

fbb3c57e104b46574e733741b2fabd56db37d4ba
ssh <sshoon812@gmail.com>
2022-03-28 22:35:04 +0900

    study

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,7 +6,11 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member", // 탐색할 패키지를 지정 할 수 있다. 이 패키지를 포함하여 하위 패키지 모두 탐색
+        basePackageClasses = AutoAppConfig.class, // 탐색할 클래스를 지정 할 수 있다.
         excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes = Configuration.class)
 )
+
+// 권장 방법 = 설정 정보 클래스의 위치를 프로젝트 최상단에 두는 것 basePackages 지정은 생략 한다.
 public class AutoAppConfig {
 }
--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -3,7 +3,7 @@ package hello.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // 컴포넌트 스캔이 포함 되어 있다.
 public class CoreApplication {
 
 	public static void main(String[] args) {

7bffbbc86fecce836e756acac6a76e8661e55f34
ssh <sshoon812@gmail.com>
2022-05-17 22:05:57 +0900

    study

---
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,61 @@
+package hello.core.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient  {
+
+// 인터페이스로 초기화 , 소멸 방법은 현재는 잘 사용허지 않음
+
+    private String url;
+
+    public NetworkClient(){
+        System.out.println("생성자 호출 url = " + url);
+
+        connect();
+        call("초기화 연결 메시지");
+
+    }
+
+
+    public void  setUrl(String url){
+        this.url=url;
+    }
+
+
+    //서비스 시작시 호출
+    public void connect(){
+        System.out.println("connect = " + url);
+    }
+
+
+    public void call(String message){
+        System.out.println("call : " +url+ "message = " + message);
+    }
+
+
+    //서비스 시작시 호출
+    public void disconnect(){
+        System.out.println("closs : " + url);
+
+    }
+
+    @PostConstruct
+    public void init() throws Exception {
+
+        connect();
+        call("초기화 연결 메시지");
+
+    }
+
+    @PreDestroy
+    public void close() throws Exception {
+        disconnect();
+    }
+
+
+
+}
